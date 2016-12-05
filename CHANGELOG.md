d7a487c80bc350d22b42637cfbb042c0a724ffd3
Jonathan Machado <jonathmachad@gmail.com>
2016-12-05 15:50:26 +0100

    Add a seed file with sample data

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+ - A seed file.
 
 ## [0.0.3] - 2016-12-05
 ### Added
--- sources/db/seeds.rb
+++ sources/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jane = User.create({name: "Jane"})
+peter = User.create({name: "Peter"})
+luke = User.create({name: "Luke"})
+
+main_office = CompanyNumber.create({name: 'main office'})
+sales = CompanyNumber.create({name: 'sales'})
+support = CompanyNumber.create({name: 'support'})
+
+main_office.users << jane << peter << luke
+sales.users << jane << luke
+support.users << peter
+
+ UserNumber.create({user: jane, name: 'desktop'})
+ UserNumber.create({user: jane, name: 'mobile'})
+ UserNumber.create({user: jane, name: 'fixed'})
+ UserNumber.create({user: peter, name: 'mobile'})
+ UserNumber.create({user: peter, name: 'desktop'})
+ UserNumber.create({user: luke, name: 'desktop'})
